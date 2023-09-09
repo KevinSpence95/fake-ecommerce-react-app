6ee0a3eead132cce1f294f603f2948fe12d0896c
Kevin Spence <KevinSpence95@gmail.com>
2023-09-09 11:55:37 -0400

    added cartLineItem. added styles for cartPage and cartLineItem.

--- src/components/pages/cartPage/cartPage.js
+++ src/components/pages/cartPage/cartPage.js
@@ -1,16 +1,28 @@
 import useShopContext from "../../../customHooks/useShopContext";
+import CartLineItem from "./components/cartLineItem";
+import styles from './cartPage.module.css'
 
 export default function CartPage() {
-  const { cart } = useShopContext();
-  return (
-    <div>
-      {cart.map((item,i) => {
-        return (
-          <p key={i}>
-            {item.id} | {item.qty}
-          </p>
-        );
-      })}
-    </div>
-  );
+  const { cart, totalPrice, totalItems } = useShopContext();
+
+  if (totalItems < 1) {
+    return (
+      <div className={styles.cartSummary}>
+        <h1>Cart Empty - Add items to proceed</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.cartSummary}>
+        <h2>Cart Summary</h2>
+        <section className={styles.lineItems}>
+          {cart.map((item, i) => {
+            return <CartLineItem key={i} cartItem={item} />;
+          })}
+        </section>
+        <h2>Subtotal : ${totalPrice}</h2>
+        <button>Proceed to checkout</button>
+      </div>
+    );
+  }
 }
--- src/components/pages/cartPage/components/cartLineItem.js
+++ src/components/pages/cartPage/components/cartLineItem.js
@@ -1,5 +1,33 @@
-export default function CartLineItem() {
+import useShopContext from "../../../../customHooks/useShopContext";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import styles from './cartLineItem.module.css'
+
+export default function CartLineItem({ cartItem }) {
+  const { products, updateQty, removeFromCart } = useShopContext();
+
+  const correspondingProd = products.find((prod) => prod.id === cartItem.id);
+
   return (
-    <div>cartLineItem</div>
-  )
+    <article className={styles.lineItem}>
+      <img src={correspondingProd.image} alt={correspondingProd.name} />
+      <h4>{correspondingProd.name}</h4>
+      <h4>${correspondingProd.price}</h4>
+      <input
+        type="number"
+        step={1}
+        min={1}
+        value={cartItem.qty}
+        onChange={(e) => {
+          updateQty(cartItem.id, e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          removeFromCart(cartItem.id);
+        }}
+      >
+        <DeleteForeverIcon style={{ fontSize: "large" }} />
+      </button>
+    </article>
+  );
 }
